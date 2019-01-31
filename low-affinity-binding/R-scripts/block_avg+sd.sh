3f1485f692744434009cc3d94090f5508682b6e3
GMKPoonLab <44706644+GMKPoonLab@users.noreply.github.com>
2019-01-31 18:31:05 -0500

    Create block_avg+sd.sh
    R script (wrapped in a shell script) for calculating block averages and standard deviations in blocks of 5

---
+++ low-affinity-binding/R-scripts/block_avg+sd.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#note- this assumes the input is divisible by 5
+
+  #prompt user for input file
+  #input should be single column only
+read -e -p "Input file: " input
+
+rm R_block.R
+
+  #make R script that reads in input as a matrix and block average over 5 blocks
+cat >R_chisq.R<<EOF
+	#read in input as table
+a <- read.table("${input}")
+
+	#change table to matrix
+b <- as.matrix(a)
+
+	#split up into matrix with 5 columns
+c <- matrix(b, nrow=(nrow(a)/5))
+
+	#calculate column means of matrix
+d <- colMeans(c)
+
+	#print mean of column means, and standard deviations
+print(paste0("Block average: ", mean(d)))
+print(paste0("Block SD: ", sd(d)))
+EOF
+
+	Rscript R_block.R
+  
+  wait
+  
+  rm R_block.R
